1c98e9b7ed15a953aa62ec5de877febcbc4b9f69
AviFS <avifsternlieb@gmail.com>
2021-07-07 18:11:57 -0700

    Cleaned up HTML, topics are now displayed alphabetically, added data to data.js, and added dark theme stylesheet

--- data.js
+++ data.js
@@ -1,18 +1,41 @@
 var topics = {
-    "brachylog": "https://github.com/JCumin/Brachylog",
-    "frink": "https://frinklang.org",
+    "Brachylog": "https://github.com/JCumin/Brachylog",
+    "Frink": "https://frinklang.org",
+    "Tulip": "https://github.com/tulip-lang/tulip",
+    "Rockstar": "https://codewithrockstar.com/",
+    "J": "https://www.jsoftware.com/#/",
+    "APL": "https://aplwiki.com/",
 }
 
 var creators = {
     "Julien C.": "https://www.youtube.com/channel/UCOCbeCJAGd558TvbHkLMFpA",
     "Confreaks": "https://www.youtube.com/channel/UCWnPjmqvljcafA0z2U1fwKQ",
+    "Strange Loop Conference": "https://www.youtube.com/c/StrangeLoopConf",
+    "Dot Next": "https://www.youtube.com/channel/UCNPwMPudMEw-gnAT4zh_UZg",
+    "tangentstorm": "https://www.youtube.com/channel/UCznGnOteb8vQ0fJyHys_OQg",
+    "Imperial College London": "https://www.youtube.com/channel/UCcRV5FPxxWFdS4Fjnk9FQvA",
+    "DyalogLtd": "https://www.youtube.com/channel/UCRFAE1uHnrhXlSkoaAgKsIQ",
 }
 
 var dataByCreator = {
     "Julien C.": [
-       ["brachylog", "A Short Introduction to Brachylog", "https://www.youtube.com/watch?v=XYVhZ-MFcUs"],
+       ["Brachylog", "A Short Introduction to Brachylog", "https://www.youtube.com/watch?v=XYVhZ-MFcUs"],
     ],
     "Confreaks": [
-        ["frink", "ELCamp 2010 - Frink", "https://www.youtube.com/watch?v=6NZ_qvQy5Cc"],
+        ["Frink", "ELCamp 2010 - Frink", "https://www.youtube.com/watch?v=6NZ_qvQy5Cc"],
+    ],
+    "Strange Loop Conference": [
+        ["Tulip", "Tulip: A Language for Humans", "https://www.youtube.com/watch?v=lvclTCDeIsY"],
+        ["J", "The J Programming Language", "https://www.youtube.com/watch?v=RWYkx6-L04Q"],
+    ],
+    "DotNext": [
+        ["Rockstar", "How I built Rockstar: Parsing esoteric languages with .NET", "https://www.youtube.com/watch?v=1jg1HqZKeKU"],
+    ],
+    "tangentstorm": [
+        ["J", "Goldbach Conjecture: Beginner J", "https://www.youtube.com/watch?v=Dh4QKfH6fTc"]
+    ],
+    "DyalogLtd": [
+        ["APL", "Conway's Game of Life", "https://www.youtube.com/watch?v=a9xAKttWgP4&t=175s"],
+        ["APL", "A Sudoku Solver in APL", "https://www.youtube.com/watch?v=DmT80OseAGs"]
     ],
 }
--- generate-html.js
+++ generate-html.js
@@ -18,20 +18,25 @@ function init() {
 
 function populateHTML() {
     var data = dataByTopic;
+    var topicNamesSorted = Object.keys(data).sort();
+
     // var data = dataByCreator;
-    var listDiv = document.getElementById("list");
-    listDiv.innerHTML = "";
-    for (var topicName in data) {
+    listHTML = "";
+    console.log(data);
+    for (const topicName of topicNamesSorted) {
         var topicLink = topics[topicName];
-        listDiv.innerHTML += `<h3><a href=${topicLink}>${topicName}</a></h3><ul>`
+        console.log(topicName);
+        listHTML += `<h3><a href=${topicLink}>${topicName}</a></h3><ul>`
         for (const videoTuple of data[topicName]) {
             var [creatorName, videoName, videoLink] = videoTuple;
             var creatorLink = creators[creatorName];
-            listDiv.innerHTML += `<ls><a href="${videoLink}">${videoName}</a> by <a href="${creatorLink}">${creatorName}</a></ls><br>`
+            listHTML += `<li><a href="${videoLink}">${videoName}</a> by <a href="${creatorLink}">${creatorName}</a></li>`
             // listDiv.innerHTML += `<ls><a href="${creatorLink}">${creatorName}</a><strong>:</strong> <a href="${videoLink}">${videoName}</a></ls><br>`
         }
-        listDiv.innerHTML += "</ul>"
+        listHTML += "</ul>"
     }
+    // The final <h3><h3> is just for the border-top styling for h3s so there's one at the bottom 
+    document.getElementById("list").innerHTML = listHTML+"<h3></h3>";
 }
 
 function populateDataByTopic() {

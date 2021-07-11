35cd329d3815ba52994a13193c39b09a5a060f48
Elsa Feher <admin@Admins-MacBook.local>
2021-07-11 00:07:52 -0700

    Added switch to toggle sorting by creator/topic. Switch css needs work.

--- generate-html.js
+++ generate-html.js
@@ -16,18 +16,14 @@ function init() {
     populateHTML();
 }
 
-function populateHTML() {
-    var data = dataByTopic;
-    var topicNamesSorted = Object.keys(data).sort();
-
-    // var data = dataByCreator;
+function dispByTopic() {
+    var topicNamesSorted = Object.keys(dataByTopic).sort();
     listHTML = "";
-    console.log(data);
     for (const topicName of topicNamesSorted) {
         var topicLink = topics[topicName];
         console.log(topicName);
         listHTML += `<h3><a href=${topicLink}>${topicName}</a></h3><ul>`
-        for (const videoTuple of data[topicName]) {
+        for (const videoTuple of dataByTopic[topicName]) {
             var [creatorName, videoName, videoLink] = videoTuple;
             var creatorLink = creators[creatorName];
             listHTML += `<li><a href="${videoLink}">${videoName}</a> by <a href="${creatorLink}">${creatorName}</a></li>`
@@ -35,6 +31,33 @@ function populateHTML() {
         }
         listHTML += "</ul>"
     }
+    return listHTML;
+}
+
+function dispByCreator() {
+    var creatorNamesSorted = Object.keys(dataByCreator).sort();
+    listHTML = "";
+    for (const creatorName of creatorNamesSorted) {
+        var creatorLink = creators[creatorName];
+        listHTML += `<h3><a href=${creatorLink}>${creatorName}</a></h3><ul>`
+        for (const videoTuple of dataByCreator[creatorName]) {
+            var [topicName, videoName, videoLink] = videoTuple;
+            var topicLink = creators[topicName];
+            listHTML += `<li><a href="${videoLink}">${videoName}</a> by <a href="${creatorLink}">${topicName}</a></li>`
+            // listDiv.innerHTML += `<ls><a href="${creatorLink}">${creatorName}</a><strong>:</strong> <a href="${videoLink}">${videoName}</a></ls><br>`
+        }
+        listHTML += "</ul>"
+    }
+    return listHTML;
+}
+
+function populateHTML() {
+
+    var isByTopic = document.getElementById("toggleSort").checked;
+    // var listHTML = dispByTopic();
+    var listHTML = isByTopic? dispByTopic(): dispByCreator();
+    
+
     // The final <h3><h3> is just for the border-top styling for h3s so there's one at the bottom 
     document.getElementById("list").innerHTML = listHTML+"<h3></h3>";
 }
